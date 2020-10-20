204b67448ee74ca47e912c447dd9c66ef615215a
gbytz <gbytz.zyt@gmail.com>
2020-10-20 19:41:09 -0300

    Add functions to lower/upper a string

--- string_utils.c
+++ string_utils.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+
 int are_equal(const char s[], const char t[])
 {
     int i;
@@ -5,3 +7,24 @@ int are_equal(const char s[], const char t[])
         ;
     return s[i] == t[i];
 }
+
+char* to_lower(char s[])
+{
+    int i;
+    for(i = 0; s[i] != '\0'; ++i)
+    {
+        s[i] = tolower(s[i]);
+    }
+    return s;
+}
+
+char* to_upper(char s[])
+{
+    int i;
+    for(i = 0; s[i] != '\0'; ++i)
+    {
+        s[i] = toupper(s[i]);
+    }
+    return s;
+}
+
